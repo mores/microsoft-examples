8a682b75548b6e436c0b3150a525feb41d392469
mores <smore@smartwattinc.com>
2017-03-04 13:19:59 -0500

    Connects to 365

--- 005-crmWhoAmI/ConsoleApplication1/Program.cs
+++ 005-crmWhoAmI/ConsoleApplication1/Program.cs
@@ -4,13 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* 
+ * First do this:
+ * CrmSvcUtil.exe /username:kingjulian /password:julianisking /url:https://julian.madagascar.com/XRMServices/2011/Organization.svc /out:CrmProxy.cs /serviceContextName:Context
+ * Object SystemUser will be in generated early bind classes
+ */
+
 namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            String url = "";
+            String user = "";
+            String password = "";
+
             Console.WriteLine("Hello, World!");
+            String connectionString = "Url=" + url + "; Username=" + user + "; Password=" + password + "; authtype=Office365";
+            Microsoft.Xrm.Tooling.Connector.CrmServiceClient conn = new Microsoft.Xrm.Tooling.Connector.CrmServiceClient(connectionString);
+            Microsoft.Xrm.Sdk.IOrganizationService _orgService = (Microsoft.Xrm.Sdk.IOrganizationService)conn.OrganizationWebProxyClient != null ? (Microsoft.Xrm.Sdk.IOrganizationService)conn.OrganizationWebProxyClient : (Microsoft.Xrm.Sdk.IOrganizationService)conn.OrganizationServiceProxy;
+            Guid userid = ((Microsoft.Crm.Sdk.Messages.WhoAmIResponse)_orgService.Execute(new Microsoft.Crm.Sdk.Messages.WhoAmIRequest())).UserId;
+            Console.WriteLine("GUID {0}.", userid);
+            SystemUser systemUser = (SystemUser)_orgService.Retrieve("systemuser", userid, new Microsoft.Xrm.Sdk.Query.ColumnSet(new string[] { "firstname", "lastname" }));
+            Console.WriteLine("Logged on user is {0} {1}.", systemUser.FirstName, systemUser.LastName);
         }
     }
 }
