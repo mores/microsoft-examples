db6d2806d69c6c60e010fc6e14f809fab7d9831e
Stephen More <smore@smartwattinc.com>
2018-03-29 18:23:48 +0100

    Adding customizations

--- MicrosoftSharePointClientWebConsole/MicrosoftSharePointClientWebConsole/Program.cs
+++ MicrosoftSharePointClientWebConsole/MicrosoftSharePointClientWebConsole/Program.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * http://www.thesharepointguide.com/access-office-365-using-a-console-application/
+ * 
+ * nuget:
+ *   AppForSharePointOnlineWebToolkit
+ */
+
+using Microsoft.SharePoint.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +18,27 @@ namespace MicrosoftSharePointClientWebConsole
     {
         static void Main(string[] args)
         {
+            Uri siteUri = new Uri("<enter url of sharepoint site here>");
+            string realm = TokenHelper.GetRealmFromTargetUrl(siteUri);
+            Console.WriteLine("...");
+            Console.WriteLine(realm);
+
+            string accessToken = TokenHelper.GetAppOnlyAccessToken(TokenHelper.SharePointPrincipal, siteUri.Authority, realm).AccessToken;
+            Console.WriteLine("...");
+            Console.WriteLine(accessToken);
+            Console.WriteLine("...");
+
+            using (var clientContext = TokenHelper.GetClientContextWithAccessToken(siteUri.ToString(), accessToken))
+            {
+                Web currentWeb = clientContext.Web;
+                clientContext.Load(currentWeb, w => w.Title, w => w.Description);
+                clientContext.ExecuteQuery();
+                Console.WriteLine(currentWeb.Title);
+                Console.WriteLine(currentWeb.Description);
+            }
+            
+            Console.WriteLine("...");
+            Console.ReadLine();
         }
     }
 }
